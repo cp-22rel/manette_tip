b6bdcba5be188f30477b857c8065f92fb5a8c33e
cp-22rel <reto.lehmann@ceff.ch>
2026-02-25 13:08:40 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -32,46 +32,53 @@ void setup()
 
 void loop()
 {
-
   if (bleGamepad.isConnected())
   {
     int16_t ax, ay, az;
     int16_t gx, gy, gz;
-
     mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    Serial.print("a/g:\t");
-    Serial.print(ax);
-    Serial.print("\t");
-    Serial.print(ay);
-    Serial.print("\t");
-    Serial.print(az);
-    Serial.print("\t");
-    Serial.print(gx);
-    Serial.print("\t");
-    Serial.print(gy);
-    Serial.print("\t");
-    Serial.println(gz);
+    // --- 1. GYRO (Speed of movement) ---
+    // Offsets applied to keep joysticks at 0 when still
+    int joyLX = map(gx + 930, -8000, 8000, -32767, 32767);
+    int joyLY = map(gy - 480, -8000, 8000, -32767, 32767);
+    int joyRZ = map(gz - 40, -8000, 8000, -32767, 32767);
 
-    int gamepadGX = map(gx, -17000, 17000, -32767, 32767);
-    int gamepadGY = map(gy, -17000, 17000, -32767, 32767);
-    int gamepadGZ = map(gz, -17000, 17000, -32767, 32767);
-    int gamepadAX = map(ax, -17000, 17000, -32767, 32767);
-    int gamepadAY = map(ay, -17000, 17000, -32767, 32767);
-    int gamepadAZ = map(az, -17000, 17000, -32767, 32767);
+    // --- 2. ACCELEROMETER (Absolute Tilt) ---
+    // We use atan2 to turn raw gravity into an angle (Radians to Degrees)
+    // This allows you to know exactly how much you are leaning.
+    float pitch = atan2(ax, sqrt(pow(ay, 2) + pow(az, 2))) * 57.296;
+    float roll = atan2(ay, az) * 57.296;
 
-    gamepadGX = constrain(gamepadGX, -32767, 32767);
-    gamepadGY = constrain(gamepadGY, -32767, 32767);
-    gamepadGZ = constrain(gamepadGZ, -32767, 32767);
-    gamepadAX = constrain(gamepadAX, -32767, 32767);
-    gamepadAY = constrain(gamepadAY, 0, 32767);
-    gamepadAZ = constrain(gamepadAZ, 0, 32767);
+    // Map angles (-90 to 90 degrees) to the Right Stick Y-axis
+    // This is your JoyRY. It tells Godot the exact lean angle.
+    int joyRY = map(pitch, -90, 90, -32767, 32767);
 
-    bleGamepad.setLeftThumb(gamepadGX, gamepadGY);
-    bleGamepad.setRightThumb(gamepadGZ, gamepadAX);
-    bleGamepad.setLeftTrigger(gamepadAY);
-    bleGamepad.setRightTrigger(gamepadAZ);
+    // --- 3. TRIGGER MAPPING ---
+    // Using the Z-axis (up/down) for a trigger.
+    // When flat, az is 16384. When vertical, az is 0.
+    int triggerR = map(az, 0, 16384, 32767, 0);
+
+    // --- 4. APPLY & SEND ---
+    bleGamepad.setLeftThumb(constrain(joyLX, -32767, 32767),
+                            constrain(joyLY, -32767, 32767));
+
+    bleGamepad.setRightThumb(constrain(joyRZ, -32767, 32767),
+                             constrain(joyRY, -32767, 32767));
+
+    bleGamepad.setRightTrigger(constrain(triggerR, 0, 32767));
+
+    Serial.print("g/a:\t");
+    Serial.print(joyLX);
+    Serial.print("\t");
+    Serial.print(joyLY);
+    Serial.print("\t");
+    Serial.print(joyRZ);
+    Serial.print("\t");
+    Serial.print(joyRY);
+    Serial.print("\t");
+    Serial.print(triggerR);
+    Serial.print("\n");
   }
-
-  delay(30);
+  delay(10);
 }
